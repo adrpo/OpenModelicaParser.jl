8d597422fc5677aaa5b1ef7bba9459abc7a26327
John <JKRT@users.noreply.github.com>
2021-04-22 14:23:31 +0200

    Create CI.yml (#3)
    * Added CI
    
    * Added MSL. Added influenza test
    
    * Updated the readme with example usage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
 import OpenModelicaParser
 
+@testset "Simple standalone modules" begin
+
 @test begin
   try
     res = OpenModelicaParser.parseFile("HelloWorld.mo")
@@ -8,4 +10,28 @@ import OpenModelicaParser
   catch
     false
   end
+end
+
+@test begin
+  try
+    res = OpenModelicaParser.parseFile("Influenza.mo")
+    true
+  catch
+    false
+  end
+end
+
+end
+
+
+@testset "Standard Library" begin
+
+@test begin
+  try
+    res = OpenModelicaParser.parseFile("msl.mo")
+    true
+  catch
+    false
+  end
+end
 end
