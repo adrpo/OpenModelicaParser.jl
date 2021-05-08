f243d27621fb30fca3a998ca1aff9711de08c161
John Tinnerholm <jtinnerholm@gmail.com>
2021-05-08 12:48:30 +0200

    Updated build script for Linux

--- deps/build.jl
+++ deps/build.jl
@@ -4,21 +4,30 @@ import ZipFile
 using HTTP
 #=Extern path=#
 PATH_TO_EXT = realpath("$(pwd())/../lib/ext")
-#= Download shared libraries =#
-HTTP.download("https://build.openmodelica.org/omc/msys2/OpenModelicaParser.jl/OpenModelicaParser-dlls.zip", PATH_TO_EXT)
-println(pwd())
-cd(PATH_TO_EXT)
-println(pwd())
-foreach(readdir()) do f
-  println("\nObject: ", f)
+@static if Sys.iswindows()
+  #= Download shared libraries (DLLS for Windows)=#
+  HTTP.download("https://build.openmodelica.org/omc/msys2/OpenModelicaParser.jl/OpenModelicaParser-dlls.zip", PATH_TO_EXT)
+  println(pwd())
+  cd(PATH_TO_EXT)
+  println(pwd())
+  foreach(readdir()) do f
+    println("\nObject: ", f)
+  end
+  @info "Unzipping archive"
+  r = ZipFile.Reader("OpenModelicaParser-dlls.zip");
+  for f in r.files
+    @info "Filename: \$(f.name)"
+    write(f.name, read(f, String));
+  end
+  @info "Unzip done. Closing file descriptor."
+  close(r)
+  @info "Done!"
+elseif Sys.islinux()
+  @info "Downloading Linux so file.."
+  HTTP.download("https://build.openmodelica.org/omc/msys2/OpenModelicaParser.jl/libomparse-julia.so", PATH_TO_EXT)
+  @info "Download done!"
+else #= Throw error for other variants =#
+  @error "Non Linux/Windows systems are currently not supported"
+  throw("Unsupported system error")
 end
-@info "Unzipping archive"
-r = ZipFile.Reader("OpenModelicaParser-dlls.zip");
-for f in r.files
-  @info "Filename: \$(f.name)"
-  write(f.name, read(f, String));
-end
-@info "Unzip done. Closing file descriptor."
-close(r)
-@info "Done!"
 
