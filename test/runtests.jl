04b2b285bcf9751fb3b41eca685f9db7d1df7c43
John Tinnerholm <jtinnerholm@gmail.com>
2021-11-29 15:04:35 +0100

    Updated build script for Linux (#5)
    * Updated build script for Linux
    
    * Disable MSL support on Linux for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,18 +20,30 @@ end
     false
   end
 end
-
-end
-
-
-@testset "Standard Library" begin
-
+  
 @test begin
   try
-    res = OpenModelicaParser.parseFile("msl.mo")
+    res = OpenModelicaParser.parseFile("Casc12800.mo")
     true
   catch
     false
   end
 end
+
+end
+
+
+@testset "Standard Library" begin
+  if Sys.iswindows()
+    @test begin
+      try
+        res = OpenModelicaParser.parseFile("msl.mo")
+        true
+      catch
+        false
+      end
+    end
+  else
+    @info "MSL is currently only supported on Windows"
+  end
 end
